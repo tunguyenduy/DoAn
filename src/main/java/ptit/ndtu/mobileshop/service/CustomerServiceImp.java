d4636e1065ffb583fa4a83a2b9cb97be0e9bffcd
NguyenDuyTu <duytu.26.96@gmail.com>
2018-09-12 16:39:07 +0700

    first commit

---
+++ src/main/java/ptit/ndtu/mobileshop/service/CustomerServiceImp.java
@@ -0,0 +1,33 @@
+package ptit.ndtu.mobileshop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ptit.ndtu.mobileshop.entities.Customer;
+import ptit.ndtu.mobileshop.repository.AccountRepository;
+import ptit.ndtu.mobileshop.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImp implements CustomerService {
+	
+	@Autowired
+	public CustomerRepository customerRepository;
+	@Autowired
+	public AccountRepository accountRepository;
+	
+	
+	@Override
+	public boolean register(Customer customer) {
+		try {
+			if(accountRepository.findByUsername(customer.getAccountId().getUsername())==null) {
+				customerRepository.save(customer);
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
